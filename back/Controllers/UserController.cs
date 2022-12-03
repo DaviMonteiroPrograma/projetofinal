82c68855a4b616e86f2f68cd919f5e2248648e74
DaviMonteiroPrograma <dalefoficial342@gmail.com>
2022-12-03 13:33:07 -0300

    É pra estar certa sa poha

--- back/Controllers/UserController.cs
+++ back/Controllers/UserController.cs
@@ -8,11 +8,62 @@ public class UserController : ControllerBase
 {
      [HttpPost("Login")]
     public async Task<IActionResult> Login(
-        [FromBody]UsuarioDTO user
-
+        [FromBody] Usuario user
     )
     {
+        using TCCSenai context = new TCCSenai ();
+
+        var possivelUser = context.Usuario.FirstOrDefault(u.UserId == user.UserId);
+
+        if(possivelUser == null){
+            return NotFound("Nome de Usuário inválido");
+        }
+        if(possivelUser.Userpass != user.Password){
+            return NotFound("Senha Inválida!");
+        }
+    }
+
+     [HttpPost("registro")]
+    public async Task<IActionResult> Registro(
+        [FromBody] Usuario user
+    )
+    {
+        using TCCSenai context = new TCCSenai ();
         
-        using  context = new ();
+        var repeatUserpass;
+
+        list<string> errors = new List<string>();
+
+        if(user.Nome.length < 5){
+            errors.Add("O nome do usuário precisa conter ao menos 5 letras");
+
+        }
+        if(user.Sobrenome.length < 5){
+            errors.Add("O Sobrenome do usuário precisa conter ao menos 5 letras");
+        }
+        if(user.Userpass.length < 5){
+            errors.Add("A senha precisa conter pelo menos 5 caracteres ");
+
+        }
+        if(user.repeatUserpass != user.Userpass){
+            errors.Add("As senhas não são iguais");
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.Name = user.Name;
+        usuario.Sobrenome = user.Sobrenome;
+        usuario.Userpass = user.Password;
+        usuario.UserId = user.UserId;
+
+        context.Add(usuario);
+        context.Savechanges();
+
+        return ok();
+    }
+
+      [HttpPost("update")]
+    public IActionResult UpdateName()
+    {
+        throw new NotImplementedException();
     }
 }
